cbbbfce769ce881696f59cfdddc44441651d8e73
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-12 14:24:28 +0530

    java program

---
+++ command/file2.java
@@ -0,0 +1,9 @@
+public class file2{
+    /*
+        1) file name
+        2)using variable
+    */
+    public static void main(String[] args){
+    System.out.println("computer programming");
+    }
+}
