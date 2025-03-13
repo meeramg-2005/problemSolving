0a65d06e452221a9a33ef88af56198e724c14222
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 11:02:38 +0530

    if else statement

---
+++ if else statement/demo1.java
@@ -0,0 +1,13 @@
+public class demo1{
+    public static void main(String[] args){
+        if(true){
+            System.out.println(12>15);
+
+        }
+        else{
+            System.out.println(12>15);
+
+        }
+        
+        }
+    }
