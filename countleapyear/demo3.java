6e620542ac8a2016a16884db5d50986bc2218be6
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 14:47:10 +0530

    counting leapyear

---
+++ countleapyear/demo3.java
@@ -0,0 +1,10 @@
+public class demo3{
+    public static void main(String args[]){
+        int count=0;
+        for(int i=2022;i<2030;i++)  
+        if(i%4==0){
+            count=count+1;
+        }
+            System.out.println(count);
+        }  
+        }
