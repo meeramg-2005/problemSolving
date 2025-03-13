56af3242d212d2d26bc9161d1dba6e63e480df82
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 14:28:54 +0530

    This is leap year

---
+++ Leapyear/demo2.java
@@ -0,0 +1,11 @@
+public class demo2{
+    public static void main(String args[]){
+        int i=2020;
+        if( i%4==0){
+            System.out.println("leapyear");
+        }
+        else{
+            System.out.println("notleapyear");
+        }
+    }
+}
