e82ba4f19825250e1ff9325fbd425e3318c9fe12
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 10:51:26 +0530

    conditional statement

---
+++ Conditional statement/demo.java
@@ -0,0 +1,11 @@
+public class demo{
+    public static void main(String[] args){
+        if(true){
+            System.out.println(12<15);
+
+        }
+        if(false){
+            System.out.println(12>15);
+        }
+    }
+}
