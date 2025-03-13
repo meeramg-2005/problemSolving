f0b97af0a6e9ace03978b5108bfaf46f13e63533
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 11:10:41 +0530

    MajorMinor calculation

---
+++ MajorMinorFinder/demo2.java
@@ -0,0 +1,14 @@
+public class demo2{
+    public static void main(String[] args){
+        int Age=20;
+        if(Age>=19){
+            System.out.println("voteValid");
+
+        }
+        else{
+            System.out.println("Not voteValid");
+        }
+
+    }
+    
+}
