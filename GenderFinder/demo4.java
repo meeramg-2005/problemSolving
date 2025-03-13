d4b6bee5fe1af00d473de77d1f56d0054e008112
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 11:36:10 +0530

    Finding Gender

---
+++ GenderFinder/demo4.java
@@ -0,0 +1,17 @@
+public class demo4{
+    public static void main(String[] args){
+        String Gender="S";
+        if(Gender=="S"){
+            System.out.println("The person is male");
+
+        }
+        if(Gender=="M"){
+            System.out.println("The person is female");
+        }
+        
+        }
+        
+        }
+
+    
+    
