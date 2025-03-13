5469bfe1ef2c0deafd2cd335261cc2c9279ef78f
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 15:10:01 +0530

    Integer value

---
+++ next int/demo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your name");
+        int Age=input.nextInt();
+        System.out.println(Age);
+    }
+}
