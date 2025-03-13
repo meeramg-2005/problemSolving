0a85d1e28a1ed63a602c3e22fc0bd7535d433c92
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 10:32:01 +0530

    conditional operators

---
+++ Conditional operator/demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String[] args){
+        System.out.println(16>10);
+        System.out.println(16<10);
+        System.out.println(16<=10);
+        System.out.println(16>=10);
+        System.out.println(16==10);
+        System.out.println(16!=10);
+    }
+}
