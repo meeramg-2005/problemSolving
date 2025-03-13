50409c3c365292ab63979acb9c9ced5280c0522b
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 14:58:43 +0530

    user name

---
+++ Scanner/demo4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class demo4{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=input.nextLine();
+        System.out.println(name);
+    }
+}
