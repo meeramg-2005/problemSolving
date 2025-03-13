c846b2d17adc886c645837a6501094781c57fe44
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-13 11:48:23 +0530

    positivenegative

--- PositiveNegative/demo3.java
+++ PositiveNegative/demo3.java
@@ -1,11 +1,16 @@
 public class demo3{
     public static void main(String[] args){
-        int num=20;
+        int num=0;
         if(num>=19){
             System.out.println("positive");
 
         }
+        else if(num==0){
+            System.out.println("Neutral");
+        }
         else{
+
+        
             System.out.println("Negative");
         }
 
