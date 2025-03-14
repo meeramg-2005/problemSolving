fdf8cca7229b49e362dd96b201f997c6627b2503
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 10:19:10 +0530

    computer system

---
+++ Table6/case3.java
@@ -0,0 +1,12 @@
+public class case3{
+    public static void main(String args[]){
+        int i=0;
+        int x=0;
+        while(i<70){
+            i=i+7;
+            x=x+1;
+            System.out.println(x+"x7="+i);
+            
+        }
+    }
+}
