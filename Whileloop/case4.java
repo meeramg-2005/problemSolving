243328fe61f445f9bee97bee87b8270505568164
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 10:04:33 +0530

    looping statement

---
+++ Whileloop/case4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class case4{
+    public static void main(String args[]){
+        int i=92;
+        while(i<=98){
+            System.out.println(i);
+            i=i+1;
+        }
+        
+    }
+}
