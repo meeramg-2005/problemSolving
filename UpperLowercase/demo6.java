1d4921edc4da0baa73c2e1b65dc613f37bb4cd10
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 12:03:39 +0530

    java program

---
+++ UpperLowercase/demo6.java
@@ -0,0 +1,7 @@
+public class demo6{
+    public static void main(String args[]){
+        String input="hello world";
+        System.out.println(input.toUpperCase());
+        System.out.println(input.toLowerCase());
+    }
+}
