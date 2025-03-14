dbfaf11728e05279dda9d9221f5d57f7f40f08e4
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 10:28:03 +0530

    operating system

---
+++ nNumbers/demo1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo1{
+    public static void main(String args[]){
+        int i=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int n=sc.nextInt();
+        while(i<n){
+            i=i+1;
+            System.out.println(i);
+
+        }
+    }
+}
