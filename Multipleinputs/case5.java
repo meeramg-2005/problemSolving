00448ec40257082a8769823de55dbac1978509a0
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 14:44:25 +0530

    java program

---
+++ Multipleinputs/case5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class case5{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the n value:");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int i=0;
+        while(i<n){
+            System.out.println("Enter the value:");
+            a[i]=sc.nextInt();
+            i=i+1;
+        }
+        while(i<n){
+            System.out.println("Enter the value:"+a[i]);
+            i=i+1;
+        }
+    }
+}
