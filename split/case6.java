374a3bbd28653925d4c97c4826a327a60fa7f74e
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 11:53:00 +0530

    java program

---
+++ split/case6.java
@@ -0,0 +1,7 @@
+public class case6{
+    public static void main(String args[]){
+        String input="hello,world";
+        String s[]=input.split(",");
+        System.out.println(s[0]);
+    }
+}
