a298e537ce714b2a92e7368ef4f6adfa85132be0
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-14 14:19:56 +0530

    java program

---
+++ Break/case6.java
@@ -0,0 +1,13 @@
+public class case6{
+    public static void main(String agrs[]){
+       int i=1;
+       while(i<10){
+        if(i%2==0){
+    if(i>5){
+        break;
+    }
+        System.out.println(i);
+    }
+    i=i+1;
+}       }
+    }
