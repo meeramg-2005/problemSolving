fd5474239d6780da1d80dba1afe92573ce8270f5
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-15 10:40:09 +0530

    java program

---
+++ Constructor/demo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class demo{
+    demo(){
+        System.out.println("Hello");
+    }    
+            public static void main(String agrs[]){
+                demo obj=new demo();
+                
+
+            }
+        }
+    
