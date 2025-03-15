5a54bbac6a60dfeeb1fafbcb12b9efac466bd9e6
meeramg-2005 <meeramgmeeramg@gmail.com>
2025-03-15 09:57:40 +0530

    java program

--- Primenumber/demo5.java
+++ Primenumber/demo5.java
@@ -2,35 +2,38 @@ import java.util.Scanner;
 
 public class demo5{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        
-        
-        boolean isPrime = true;
+        int number = sc.nextInt();
         
     
         if (number <= 1) {
-            isPrime = false;
+            System.out.println("It is not prime number");
         } else {
+            int count=0;
             for (int i = 2; i <= number / 2; i++) {
                 if (number % i == 0) {
-                    isPrime = false;
+                    count=count+1;
                     break;
                 }
+                    
+                    
+                }
+                if(count==0){
+
+                 
+                System.out.println("It is  prime number");
+                }
+                else{
+                    System.out.println("It is not a prime number");
+                }
+
+                
             }
         }
         
         
-        if (isPrime) {
-            System.out.println(number + " is a prime number.");
-        } else {
-            System.out.println(number + " is not a prime number.");
-        }
         
-        scanner.close();
     }
-}
+
 
